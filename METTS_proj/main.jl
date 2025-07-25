420c3dd034ccbd553aaefb2ea6485d0bb6c6a87e
gautham <gautham.sathish@campus.lmu.de>
2025-07-25 21:05:25 +0200

    final

--- METTS_proj/main.jl
+++ METTS_proj/main.jl
@@ -1,3 +1,14 @@
+import tn_julia: svd, contract, updateLeft, identity, permutedims, spinlocalspace, reshape, rightcanonical, leftcanonical, mpo_expectation, applyHtoC, computeLeftEnvironment, computeRightEnvironment, sitecanonical, canonForm
+using Plots
+include("basics.jl")
+include("states.jl")
+include("gates.jl")
+include("collapse.jl")
+
+
+
+using JLD2
+
 L = 50
 beta = 1
 steps = 10
@@ -5,22 +16,44 @@ ensemble_size = 15
 Nkeep = 30
 Nsteps = beta*150
 
-savemat = zeros(ensemble_size,steps)
+savemat = Matrix{Any}(undef, ensemble_size, steps)
+
 function createEnsemble()
+    
     for e in 1:ensemble_size
-        cps = cpsrandom(1,L) #new seed for each loop
-        cps_xz = copy(cps)
-        cps_z = copy(cps)
-        for k in 1:steps
-            metts = tdmrg(cps_xz,beta,Nsteps,Nkeep)
-            savemat[e,k] = metts
-            writedlm("output_test.txt", savemat)
-            if rolls > 0.5
-                cps_xz = cpscollapse(metts,1)
-            else
-                cps_xz = cpscollapse(metts,3)
+        println("starting ensemble $e")
+        success = false
+        trial = 0
+        max_trials = 10
+
+        while !success && trial < max_trials
+            try
+                cps = cpsrandom(1,L) #new seed for each loop
+                cps_xz = copy(cps)
+
+                for k in 1:steps
+                    metts = tdmrg(cps_xz,beta,Nsteps,Nkeep) #time evolve to create metts 
+                    savemat[e,k] = metts #save metts to row e, column k
+                    println("saved ensemble $e, metts $k")
+                    rolls = rand()
+                    if rolls > 0.5 #random collapse is new cps
+                        cps_xz = cpscollapse(metts,1)
+                    else
+                        cps_xz = cpscollapse(metts,3)
+                    end
+                end
+                success = true
+            catch
+                trial += 1
+                println("Error in ensemble $e, trial $trial, retrying")
             end
         end
+        @save "output_betaIs1.jld2" savemat
     end
-
 end
+
+createEnsemble()
+
+
+
+
